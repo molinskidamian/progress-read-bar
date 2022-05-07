5a0ca9e310713b5b0953be3a9bb94b54f9cc5f72
Damian Molinski <tedeer@gmail.com>
2022-05-07 23:42:21 +0200

    Base structure project and addEventListener - scroll

---
+++ js/index.js
@@ -0,0 +1,6 @@
+const readProgress = document.querySelector('.read-progress');
+const wrapper = document.querySelector('.wrapper');
+
+window.addEventListener('scroll', function (e) {
+	console.log(window.scrollY);
+});
