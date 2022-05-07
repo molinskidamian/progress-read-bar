c0a4578fb421c62b2b17511de943c42d8fcff1d8
Damian Molinski <tedeer@gmail.com>
2022-05-08 00:20:10 +0200

    Prototype 1

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 const readProgress = document.querySelector('.read-progress');
 const wrapper = document.querySelector('.wrapper');
+let value = 0;
+
+function calculate(pageHeight) {
+	return Math.floor((100 * value) / pageHeight);
+}
 
 window.addEventListener('scroll', function (e) {
-	console.log(window.scrollY);
+	// value = window.scrollY;
+	value = window.scrollY;
+	console.log(`value: ${value}px - ${calculate(document.body.clientHeight)}%`);
+
+	if (value > 0) {
+		readProgress.classList.add('read-progress--show');
+		wrapper.classList.add('wrapper-margin--add');
+		readProgress.setAttribute('value', calculate(document.body.clientHeight));
+	} else {
+		readProgress.classList.remove('read-progress--show');
+		wrapper.classList.remove('wrapper-margin--add');
+	}
 });
